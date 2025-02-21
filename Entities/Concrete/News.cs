466b0eff261144ad99c6a3db228b6419c2288133
Hasan Can Sevim <shasancan0@gmail.com>
2025-02-21 20:25:11 +0300

    21.02.2025

--- Entities/Concrete/News.cs
+++ Entities/Concrete/News.cs
@@ -8,7 +8,7 @@ namespace Entities.Concrete
         public int Id { get; set; }
         public string Title { get; set; }
         public string Content { get; set; }
-        public string ImageUrl { get; set; }
+        public string? ImageUrl { get; set; }
         public DateTime PublishDate { get; set; }
         public int CategoryId { get; set; }
         public int AuthorId { get; set; }

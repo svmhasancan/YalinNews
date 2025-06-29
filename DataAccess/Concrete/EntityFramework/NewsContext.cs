d4699f2f43c48898813b2c19978e2cc97982cf1b
Hasan Can Sevim <shasancan0@gmail.com>
2025-06-29 19:39:12 +0300

    29.06.2025 commit 3

--- DataAccess/Concrete/EntityFramework/NewsContext.cs
+++ DataAccess/Concrete/EntityFramework/NewsContext.cs
@@ -1,15 +1,32 @@
 using Core.Entities.Concrete;
 using Entities.Concrete;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace DataAccess.Concrete.EntityFramework
 {
     public class NewsContext : DbContext
     {
+        private readonly IConfiguration _configuration;
+
+        public NewsContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=YalinNews;Trusted_Connection=true");
+            if (!optionsBuilder.IsConfigured)
+            {
+                var connectionString = _configuration.GetConnectionString("DefaultConnection");
+                optionsBuilder.UseSqlServer(connectionString);
+            }
         }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=YalinNews;Trusted_Connection=true");
+        //}
 
         public DbSet<News> News { get; set; }
         public DbSet<Category> Categories { get; set; }
